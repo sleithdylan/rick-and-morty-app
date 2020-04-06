49d478abfe56fdd57dcc25359482e1fcde457c16
Dylan Sleith <sleithdylan@gmail.com>
2020-04-07 00:20:24 +0100

    Remove component

--- src/app/tab3/tab3.module.ts
+++ src/app/tab3/tab3.module.ts
@@ -4,16 +4,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab3Page } from './tab3.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 @NgModule({
-  imports: [
-    IonicModule,
-    CommonModule,
-    FormsModule,
-    ExploreContainerComponentModule,
-    RouterModule.forChild([{ path: '', component: Tab3Page }])
-  ],
-  declarations: [Tab3Page]
+  imports: [IonicModule, CommonModule, FormsModule, RouterModule.forChild([{ path: '', component: Tab3Page }])],
+  declarations: [Tab3Page],
 })
 export class Tab3PageModule {}
